9f185d6999b2dc170426cd8be1ad632c8406f3ce
Mohamad Rabbani <mrabbani@outlook.com>
2024-01-20 17:17:12 +0500

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
 import React from "react";
-// Import Router
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Store from "./views/Store";
-import ProductView from "./views/ProductView";
-import Cart from "./views/Cart";
-import Signin from "./views/Signin";
-import Signup from "./views/Signup";
-import HomePage from "./views/HomePage";
-import OrdersPage from "./views/OrdersPage";
-import StoreApi from "./views/StoreApi";
-import SettingsPage from "./views/SettingsPage";
-import ProductPage from "./views/ProductPage";
-import CreateProductPage from "./views/CreateProductPage";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/store" element={<Store />}></Route>
-        <Route path="/product" element={<ProductPage />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/orders" element={<OrdersPage />}></Route>
-        <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/storeapi" element={<CreateProductPage />}></Route>
-        <Route path="/settings" element={<SettingsPage />}></Route>
-      </Routes>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+// import App from "./App.jsx";
+import "./styles/index.css";
+// Import Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Store from "./views/Store";
+import Cart from "./views/Cart";
+import Signin from "./views/Signin";
+import Signup from "./views/Signup";
+import HomePage from "./views/HomePage";
+import OrdersPage from "./views/OrdersPage";
+import SettingsPage from "./views/SettingsPage";
+import ProductPage from "./views/ProductPage";
+import CreateProductPage from "./views/CreateProductPage";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/store" element={<Store />}></Route>
+        <Route path="/product" element={<ProductPage />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/orders" element={<OrdersPage />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/storeapi" element={<CreateProductPage />}></Route>
+        <Route path="/settings" element={<SettingsPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
