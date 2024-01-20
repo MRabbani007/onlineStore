3120f86517abbf7e4d191e1960ddf3f6082957f0
Mohamad Rabbani <mrabbani@outlook.com>
2024-01-20 21:43:13 +0500

    bug fix

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Imported Styles
-import "./index.css";
+import "./styles/index.css";
 // Imported Components
 import Store from "./views/Store";
 import Cart from "./views/Cart";
